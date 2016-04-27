856f1805c9bf3edded15c2260d67310f42cc1b25
Yadikaer Yasheng <yadikae1@umbc.edu>
2016-04-27 15:39:45 -0400

    add in sprites to prefabs too
    - prefabs batch creation

---
+++ SpritesToPrefab/SpriteToPrefab.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteToPrefab : MonoBehaviour
+{
+    public Sprite[] sprites; //{ get; private set; }
+    public string resPath = "";
+    public string desPath = "Prefabs/";
+
+    public void LoadSprite()
+    {
+        sprites = Resources.LoadAll<Sprite>(resPath);
+    }
+
+}
---
+++ SpritesToPrefab/SpriteToPrefabEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SpriteToPrefab))]
+public class SpriteToPrefabEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SpriteToPrefab editor = (SpriteToPrefab)target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Load & Create"))
+        {
+            editor.LoadSprite();
+
+            for (int i = 0; i < editor.sprites.Length; i++)
+            {
+                //var localPath : String = "Assets/" + go.name + ".prefab";
+                string path = "Assets/" + editor.desPath + editor.sprites[i].name + ".prefab";
+
+                GameObject obj = new GameObject(editor.sprites[i].name);
+                obj.AddComponent<SpriteRenderer>().sprite = editor.sprites[i];
+                PrefabUtility.CreatePrefab(path, obj);
+
+                DestroyImmediate(obj);
+            }
+        }
+    }
+}
